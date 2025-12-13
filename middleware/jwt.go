9d7bf0603b39c67b1965f611c3dc5dca794af36c
Half_nothing <Half_nothing@163.com>
2025-12-13 15:40:08 +0800

    fix(jwt): 改进JWT错误处理逻辑
    - 添加对echojwt.TokenExtractionError的解包处理
    - 扩展缺失或格式错误JWT的错误判断条件
    - 统一错误响应格式，提高错误处理一致性
    - 优化JWT验证中间件配置结构

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -40,32 +40,42 @@ func GetJWTMiddleware(
 	requireNoRefreshToken echo.MiddlewareFunc,
 	requireRefreshToken echo.MiddlewareFunc,
 ) {
-	jwtConfig := echojwt.Config{
-		TokenLookup: "header:Authorization:Bearer ",
-		ParseTokenFunc: func(c echo.Context, auth string) (interface{}, error) {
-			if auth == "" {
-				return nil, ErrMissingOrMalformedJwt
-			}
-			token := factory.EmptyClaim()
-			_, err := factory.VerifyJwt(auth, token)
-			if err != nil {
-				return nil, err
-			}
-			return token, nil
-		},
-		ErrorHandler: func(c echo.Context, err error) error {
-			var data *dto.ApiResponse[any]
-			switch {
-			case errors.Is(err, jwt.ErrTokenMalformed) || errors.Is(err, ErrMissingOrMalformedJwt):
-				data = dto.NewApiResponse[any](dto.ErrMissingOrMalformedJwt, nil)
-			case errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenInvalidClaims):
-				data = dto.NewApiResponse[any](dto.ErrInvalidOrExpiredJwt, nil)
-			default:
-				data = dto.NewApiResponse[any](dto.ErrUnknownJwtError, nil)
-			}
-			return data.Response(c)
-		},
+	jwtConfig := echojwt.Config{}
+	jwtConfig.TokenLookup = "header:Authorization:Bearer "
+	jwtConfig.ParseTokenFunc = func(c echo.Context, auth string) (interface{}, error) {
+		if auth == "" {
+			return nil, ErrMissingOrMalformedJwt
+		}
+		token := factory.EmptyClaim()
+		_, err := factory.VerifyJwt(auth, token)
+		if err != nil {
+			return nil, err
+		}
+		return token, nil
 	}
+	jwtConfig.ErrorHandler = func(c echo.Context, err error) error {
+		var data *dto.ApiResponse[any]
+		var e *echojwt.TokenExtractionError
+		if errors.As(err, &e) {
+			err = e.Unwrap()
+		}
+		switch {
+		case errors.Is(err, jwt.ErrTokenMalformed):
+			fallthrough
+		case errors.Is(err, ErrMissingOrMalformedJwt):
+			fallthrough
+		case err.Error() == "missing value in request header":
+			data = dto.NewApiResponse[any](dto.ErrMissingOrMalformedJwt, nil)
 
+		case errors.Is(err, jwt.ErrTokenExpired):
+			fallthrough
+		case errors.Is(err, jwt.ErrTokenInvalidClaims):
+			data = dto.NewApiResponse[any](dto.ErrInvalidOrExpiredJwt, nil)
+
+		default:
+			data = dto.NewApiResponse[any](dto.ErrUnknownJwtError, nil)
+		}
+		return data.Response(c)
+	}
 	return echojwt.WithConfig(jwtConfig), jwtVerifyMiddleWare(false), jwtVerifyMiddleWare(true)
 }
